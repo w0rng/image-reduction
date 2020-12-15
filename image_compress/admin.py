0ee6dbafac9c8f274ceb80624744b0a7b7880909
w0rng <antonabramov2000@gmail.com>
2020-12-15 16:44:22 +0700

    Модель объекта на картинке

--- image_compress/admin.py
+++ image_compress/admin.py
@@ -4,3 +4,8 @@ from . import models
 @admin.register(models.Data)
 class Data(admin.ModelAdmin):
     list_display = ('id', 'array')
+
+
+@admin.register(models.Object)
+class Object(admin.ModelAdmin):
+    list_display = ('id', 'name')
--- image_compress/models.py
+++ image_compress/models.py
@@ -6,5 +6,20 @@ class Data(models.Model):
     array = ArrayField(
             models.IntegerField(),
             size=CODE_SIZE,
-            verbose_name='Закодированное изображение'
+            verbose_name='Данные'
         )
+    
+    class Meta:
+        verbose_name = 'Закодированное изображение'
+        verbose_name_plural = 'Закодированные изображения'
+
+
+class Object(models.Model):
+    name = models.CharField('Название', max_length=255)
+
+    class Meta:
+        verbose_name = 'Объект'
+        verbose_name_plural = 'Объекты'
+
+    def __str__(self):
+        return self.name
